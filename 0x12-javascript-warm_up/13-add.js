231598dba6288c6059370b1076eb9ea6ef7f18cb
bilisumafromDZ <bilisuma.ambisa@gmail.com>
2024-02-12 21:42:20 +0300

    javascript project

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,17 +1,4 @@
 #!/usr/bin/node
-let myObject = {
-	type: 'object',
-	value: 12
+exports.add = function (a, b) {
+  return (a + b);
 };
-console.log(myObject);
-
-myObject.incr = function () {
-	this.value++;
-};
-
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
