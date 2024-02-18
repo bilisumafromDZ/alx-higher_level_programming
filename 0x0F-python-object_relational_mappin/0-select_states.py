9e2ed16e44ee3f35f6cad1451f636e40184f1430
bilisumafromDZ <bilisuma.ambisa@gmail.com>
2024-02-18 13:08:17 +0300

    project using python on more objects

--- 0x0F-python-object_relational_mappin/0-select_states.py
+++ 0x0F-python-object_relational_mappin/0-select_states.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+"""
+SelectStates module
+"""
 import MySQLdb
 import sys
 
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+def select_states():
+    """Grabs states from database"""
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=database
+                         )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute('SELECT * FROM states ORDER BY id ASC')
     rows = cur.fetchall()
     for row in rows:
         print(row)
     cur.close()
     db.close()
+
+if __name__ == "__main__":
+    select_states()
